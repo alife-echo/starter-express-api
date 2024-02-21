a720aba3498966ece2c8d59c08fe5c8fbb5aa24f
alife-echo <alifesilva90@gmail.com>
2024-02-21 10:04:47 -0300

    config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    console.log("Meu zovo")
     res.send('Meu zovo')
 })
 app.listen(process.env.PORT || 3000)
